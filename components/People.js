b95344b182d19e5cf975518a2fc2fe59f89cbb3a
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2022-03-22 14:30:42 -0700

    Update properties displayed

--- components/People.js
+++ components/People.js
@@ -13,13 +13,7 @@ export default function People() {
           <Picture src={person.photo} size="75vw" />
         </td>
         <td>{person.firstName}</td>
-        <td>
-          {person.searchEndDate
-            ? helper.daysElapsed(new Date(2022, 3, 19), person.searchEndDate)
-            : 'N/A'}
-        </td>
-        <td>{person.stack ? person.stack : 'N/A'}</td>
-        <td>{person.industry ? person.industry : 'N/A'}</td>
+        <td>{person.company}</td>
       </tr>
     );
   });
@@ -31,9 +25,7 @@ export default function People() {
         <thead>
           <tr>
             <th colSpan="2"></th>
-            <th>Length</th>
-            <th>Stack</th>
-            <th>Industry</th>
+            <th>Company</th>
           </tr>
         </thead>
         <tbody>{people}</tbody>
