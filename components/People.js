a3cd94a8ea95b7ae0edda5b167e1ef5aaddc8344
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2022-03-23 16:32:31 -0700

    Switch to next/Image

--- components/People.js
+++ components/People.js
@@ -3,6 +3,7 @@ import data from '../data.js';
 import helper from '../helper.js';
 
 import Picture from './Picture.js';
+import Image from 'next/image'
 import Spacer from './Spacer.js';
 
 export default function People() {
@@ -14,7 +15,10 @@ export default function People() {
         </td>
         <td>{`${person.firstName} ${person.lastName.slice(0, 1)}.`}</td>
         <td>{person.role ? person.role : 'N/A'}</td>
-        <td>{person.company? <img src={person.company} alt="Company Logo" height="64px"/> : 'Still Looking!'}</td>
+        <td>{person.companyLogo ?
+          <div className={styles.imageWrapper}>
+            <Image src={person.companyLogo} alt="Company Logo" layout="fill" objectFit="contain"/>
+          </div> : 'Still Looking!'}</td>
       </tr>
     );
   });
--- components/Picture.js
+++ components/Picture.js
@@ -1,14 +1,13 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 
 export default function Photo(props) {
   return (
-    <img
+    <Image
       src={props.src}
       alt="portrait"
       width={props.width ? props.width : props.size}
       height={props.height ? props.height : props.size}
       loading="lazy"
-      style={{ border: 'none', padding: '0' }}
     />
   );
 }
